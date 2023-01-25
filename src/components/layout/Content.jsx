862fe81940405edc01e5744e91eb337b08d9dd77
cristian_santos <cristian_santos@git>
2023-01-25 15:45:49 -0300

    add_router_examples

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Content.css'
+import { Routes, Route } from "react-router-dom";
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
+
+const Content = props => {
+    return (
+        <main className="Content">
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/param/:id" element={<Param />} />
+                <Route exact path="/about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </main>
+    )
+}
+
+export default Content
